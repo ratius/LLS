3551cd4446fd9286d5cf01516aa7994dc2e34870
ratius <36814202+ratius@users.noreply.github.com>
2020-07-06 19:32:27 +0900

    Add files via upload

--- data/events.js
+++ data/events.js
@@ -4,134 +4,153 @@ const EventList = [
 {
 	"id" : 1,
 	"name" : "秘密のパーティー！",
-	"date" : "2019/10/03",
-	"duration" : 12,
+	"type" : "story",
+	"started" : new Date("2019-10-03T15:00:00+0900"),
+	"ended"   : new Date("2019-10-15T14:59:59+0900"),
 	"participated" : ["Honoka", "Kotori", "Chika", "You", "Ayumu"]
 },
 {
 	"id" : 2,
 	"name" : "和装モデルはお任せあれ！",
-	"date" : "2019/10/21",
-	"duration" : 10,
+	"type" : "story",
+	"started" : new Date("2019-10-21T15:00:00+0900"),
+	"ended"   : new Date("2019-11-01T14:59:59+0900"),
 	"participated" : ["Nico", "Maki", "Dia", "Ruby", "Kasumi"]
 },
 {
 	"id" : 3,
 	"name" : "下町巡り珍道中",
-	"date" : "2020/11/06",
-	"duration" : 9,
+	"type" : "story",
+	"started" : new Date("2019-11-06T15:00:00+0900"),
+	"ended"   : new Date("2019-11-15T14:59:59+0900"),
 	"participated" : ["Rin", "Hanayo", "Kanan", "Mari", "Ai"]
 },
 {
 	"id" : 4,
 	"name" : "ハイキングでリフレッシュ！",
-	"date" : "2020/11/21",
-	"duration" : 9,
+	"type" : "story",
+	"started" : new Date("2019-11-21T15:00:00+0900"),
+	"ended"   : new Date("2019-11-30T14:59:59+0900"),
 	"participated" : ["Eli", "Umi", "Yoshiko", "Hanamaru", "Kanata"]
 },
 {
 	"id" : 5,
 	"name" : "素敵なところへご招待！",
-	"date" : "2020/12/06",
-	"duration" : 10,
+	"type" : "story",
+	"started" : new Date("2020-12-06T15:00:00+0900"),
+	"ended"   : new Date("2020-12-16T14:59:59+0900"),
 	"participated" : ["Nozomi", "Hanayo", "Chika", "Riko", "Emma"]
 },
 {
 	"id" : 6,
 	"name" : "スクールアイドルトレイン発車！",
-	"date" : "2020/12/23",
-	"duration" : 8,
+	"type" : "story",
+	"started" : new Date("2020-12-23T15:00:00+0900"),
+	"ended"   : new Date("2020-12-31T14:59:59+0900"),
 	"participated" : ["Honoka", "Nico", "Dia", "Mari", "Rina"]
 },
 {
 	"id" : 7,
 	"name" : "海の上の大熱戦",
-	"date" : "2020/01/06",
-	"duration" : 9,
+	"type" : "exchange",
+	"started" : new Date("2020-01-06T15:00:00+0900"),
+	"ended"   : new Date("2020-01-15T14:59:59+0900"),
 	"participated" : ["Umi", "Maki", "You", "Hanamaru", "Shizuku", "Karin"]
 },
 {
 	"id" : 8,
 	"name" : "スペシャルスイーツを召し上がれ♡",
-	"date" : "2020/01/22",
-	"duration" : 9,
+	"type" : "story",
+	"started" : new Date("2020-01-22T15:00:00+0900"),
+	"ended"   : new Date("2020-01-31T14:59:59+0900"),
 	"participated" : ["Eli", "Nozomi", "Kanan", "Ruby", "Ayumu", "Setsuna"]
 },
 {
 	"id" : 9,
 	"name" : "二人の奏でる音",
-	"date" : "2020/02/06",
-	"duration" : 8,
+	"type" : "exchange",
+	"started" : new Date("2020-02-06T15:00:00+0900"),
+	"ended"   : new Date("2020-02-14T14:59:59+0900"),
 	"participated" : ["Eli", "Maki", "Riko", "Yoshiko", "Kasumi", "Karin"]
 },
 {
 	"id" : 10,
 	"name" : "それゆけUMA捜索隊",
-	"date" : "2020/02/20",
-	"duration" : 8,
+	"type" : "story",
+	"started" : new Date("2020-02-20T15:00:00+0900"),
+	"ended"   : new Date("2020-02-28T14:59:59+0900"),
 	"participated" : ["Honoka", "Hanayo", "Kanan", "You", "Ayumu", "Ai"]
 },
 {
 	"id" : 11,
 	"name" : "いたずらオオカミを捕まえろ！",
-	"date" : "2020/03/06",
-	"duration" : 10,
+	"type" : "exchange",
+	"started" : new Date("2020-03-06T15:00:00+0900"),
+	"ended"   : new Date("2020-03-16T14:59:59+0900"),
 	"participated" : ["Umi", "Rin", "Chika", "Ruby", "Setsuna", "Emma"]
 },
 {
 	"id" : 12,
 	"name" : "マジカル・タイム！",
-	"date" : "2020/03/23",
-	"duration" : 8,
+	"type" : "story",
+	"started" : new Date("2020-03-23T15:00:00+0900"),
+	"ended"   : new Date("2020-03-31T14:59:59+0900"),
 	"participated" : ["Kotori", "Nozomi", "Riko", "Dia", "Kasumi", "Shizuku"]
 },
 {
 	"id" : 13,
 	"name" : "ベジタブル☆クッキング！",
-	"date" : "2020/04/06",
-	"duration" : 9,
+	"type" : "exchange",
+	"started" : new Date("2020-04-06T15:00:00+0900"),
+	"ended"   : new Date("2020-04-15T14:59:59+0900"),
 	"participated" : ["Eli", "Kotori", "Dia", "Yoshiko", "Emma", "Rina"]
 },
 {
 	"id" : 14,
 	"name" : "青春のアイススケート",
-	"date" : "2020/04/21",
-	"duration" : 9,
+	"type" : "story",
+	"started" : new Date("2020-04-21T15:00:00+0900"),
+	"ended"   : new Date("2020-04-30T14:59:59+0900"),
 	"participated" : ["Rin", "Maki", "You", "Mari", "Kasumi", "Setsuna"]
 },
 {
 	"id" : 15,
 	"name" : "温泉旅行ラプソディ",
-	"date" : "2020/05/07",
-	"duration" : 8,
+	"type" : "exchange",
+	"started" : new Date("2020-05-07T15:00:00+0900"),
+	"ended"   : new Date("2020-05-15T14:59:59+0900"),
 	"participated" : ["Nozomi", "Nico", "Chika", "Hanamaru", "Shizuku", "Kanata"]
 },
 {
 	"id" : 16,
 	"name" : "取り戻せ！笑顔のラーメン",
-	"date" : "2020/05/21",
-	"duration" : 8,
+	"type" : "story",
+	"started" : new Date("2020-05-21T15:00:00+0900"),
+	"ended"   : new Date("2020-05-29T14:59:59+0900"),
 	"participated" : ["Honoka", "Rin", "Kanan", "Dia", "Ayumu", "Rina"]
 },
 {
 	"id" : 17,
 	"name" : "3人のプリンセス",
-	"date" : "2020/06/05",
-	"duration" : 8,
+	"type" : "exchange",
+	"started" : new Date("2020-06-05T15:00:00+0900"),
+	"ended"   : new Date("2020-06-13T14:59:59+0900"),
 	"participated" : ["Honoka", "Maki", "Riko", "You", "Karin", "Setsuna"]
 },
 {
 	"id" : 18,
 	"name" : "君と、雨に歌えば",
-	"date" : "2020/06/22",
-	"duration" : 8,
+	"type" : "story",
+	"started" : new Date("2020-06-22T15:00:00+0900"),
+	"ended"   : new Date("2020-06-30T14:59:59+0900"),
 	"participated" : ["Nozomi", "Nico", "Kanan", "Hanamaru", "Ai", "Emma"]
 },
 {
 	"id" : 19,
 	"name" : "ヨハネと花陽の事件簿",
-	"date" : "2020/07/06",
-	"duration" : 8,
+	"type" : "exchange",
+	"started" : new Date("2020-07-06T15:00:00+0900"),
+	"ended"   : new Date("2020-07-14T14:59:59+0900"),
 	"participated" : ["Eli", "Hanayo", "Kanan", "Yoshiko", "Ayumu", "Kanata"]
 }
 ];
