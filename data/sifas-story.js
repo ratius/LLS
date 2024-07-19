9c200c7e6fa61a5b1cee15560ad674b82186de16
ratius <36814202+ratius@users.noreply.github.com>
2024-07-20 00:41:31 +0900

    Add files via upload

---
+++ data/sifas-story.js
@@ -0,0 +1,206 @@
+const TagData = {
+	"main":       {"name": "メイン",  "r":122, "g":202, "b":223, "style": "square"},
+	"main2":      {"name": "メイン",  "r":160, "g":140, "b":240, "style": "square"},
+	"event":      {"name": "イベント","r":255, "g":130, "b":127, "style": "square"},
+	"kizuna":     {"name": "キズナ",  "r":119, "g":191, "b":220, "style": "square"},
+	"special":    {"name": "特殊",    "r":240, "g":192, "b":128, "style": "square"},
+
+	"Honoka":     {"name": "穂乃果",  "r":255, "g":163, "b": 54, "style": "round"},
+	"Eli":        {"name": "絵里",    "r":122, "g":238, "b":255, "style": "round"},
+	"Kotori":     {"name": "ことり",  "r":206, "g":191, "b":191, "style": "round"},
+	"Umi":        {"name": "海未",    "r": 23, "g":105, "b":255, "style": "round"},
+	"Rin":        {"name": "凛",      "r":219, "g":212, "b": 30, "style": "round"},
+	"Maki":       {"name": "真姫",    "r":255, "g": 80, "b": 62, "style": "round"},
+	"Nozomi":     {"name": "希",      "r":196, "g": 85, "b":246, "style": "round"},
+	"Hanayo":     {"name": "花陽",    "r":106, "g":230, "b":115, "style": "round"},
+	"Nico":       {"name": "にこ",    "r":255, "g": 79, "b":145, "style": "round"},
+	"Chika":      {"name": "千歌",    "r":255, "g":149, "b": 71, "style": "round"},
+	"Riko":       {"name": "梨子",    "r":255, "g":158, "b":172, "style": "round"},
+	"Kanan":      {"name": "果南",    "r": 39, "g":193, "b":183, "style": "round"},
+	"Dia":        {"name": "ダイヤ",  "r":219, "g":  8, "b": 57, "style": "round"},
+	"You":        {"name": "曜",      "r":102, "g":192, "b":255, "style": "round"},
+	"Yoshiko":    {"name": "善子",    "r":193, "g":202, "b":212, "style": "round"},
+	"Hanamaru":   {"name": "花丸",    "r":255, "g":208, "b": 16, "style": "round"},
+	"Mari":       {"name": "鞠莉",    "r":194, "g": 82, "b":198, "style": "round"},
+	"Ruby":       {"name": "ルビィ",  "r":255, "g":111, "b":190, "style": "round"},
+	"Ayumu":      {"name": "歩夢",    "r":255, "g":191, "b":224, "style": "round"},
+	"Kasumi":     {"name": "かすみ",  "r":213, "g":222, "b":112, "style": "round"},
+	"Shizuku":    {"name": "しずく",  "r":187, "g":237, "b":255, "style": "round"},
+	"Karin":      {"name": "果林",    "r": 74, "g": 47, "b":237, "style": "round"},
+	"Ai":         {"name": "愛",      "r":255, "g":130, "b": 70, "style": "round"},
+	"Kanata":     {"name": "彼方",    "r":190, "g":130, "b":255, "style": "round"},
+	"Setsuna":    {"name": "せつ菜",  "r":246, "g": 14, "b": 14, "style": "round"},
+	"Emma":       {"name": "エマ",    "r":143, "g":218, "b":121, "style": "round"},
+	"Rina":       {"name": "璃奈",    "r":208, "g":206, "b":225, "style": "round"},
+	"Shioriko":   {"name": "栞子",    "r": 36, "g":189, "b":139, "style": "round"},
+	"Mia":        {"name": "ミア",    "r":214, "g":213, "b":202, "style": "round"},
+	"Lanzhu":     {"name": "ランジュ","r":248, "g":200, "b":196, "style": "round"},
+	"Player":     {"name": "あなた",  "r":160, "g":192, "b":160, "style": "round"},
+
+	"Yukiho":     {"name": "雪穂",        "r":128, "g": 80, "b": 96, "style": "round"},
+	"Arisa":      {"name": "亜里沙",      "r":128, "g": 80, "b": 96, "style": "round"},
+	"Yoitsumu":   {"name": "よいつむ",    "r": 80, "g":112, "b":128, "style": "round"},
+	"Mito":       {"name": "美渡",        "r": 80, "g":112, "b":128, "style": "round"},
+	"Uchicchi":   {"name": "うちっちー",  "r": 80, "g":112, "b":128, "style": "round"},
+	"Haruka":     {"name": "遥",          "r":128, "g":112, "b": 80, "style": "round"},
+	"Misato":     {"name": "美里",        "r":128, "g":112, "b": 80, "style": "round"},
+	"Mai":        {"name": "マイ",        "r":128, "g":112, "b": 80, "style": "round"},
+	"Kaoruko":    {"name": "薫子",        "r":128, "g":112, "b": 80, "style": "round"},
+	"Uzuki":      {"name": "右月",        "r":128, "g":112, "b": 80, "style": "round"},
+	"Satsuki":    {"name": "左月",        "r":128, "g":112, "b": 80, "style": "round"},
+	"Hanpen":     {"name": "はんぺん",    "r":128, "g":112, "b": 80, "style": "round"},
+	"Rijicho":    {"name": "理事長",      "r":128, "g":112, "b": 80, "style": "round"},
+	"Asagi":      {"name": "浅希",        "r":128, "g":112, "b": 80, "style": "round"},
+	"Acting":     {"name": "演劇部部長"  ,"r":128, "g":112, "b": 80, "style": "round"},
+	"Basketball": {"name": "バスケ部部長","r":128, "g":112, "b": 80, "style": "round"},
+	"Tsumugi":    {"name": "ツムギ",      "r":128, "g":112, "b": 80, "style": "round"},
+};
+
+//■プルダウンメニューを操作した時の処理
+function NarrowerEvent(changed){
+	const temp1 = document.getElementById('N1');
+	const temp1index = temp1.selectedIndex -1;
+	const temp1list = window['JSON-sifas-story'][temp1index];
+	const temp2 = document.getElementById('N2');
+	const temp2index = temp2.selectedIndex -1;
+
+	switch (changed){
+		case 1:
+			if (temp1index < 0){
+				temp2.style.display ="none";
+			} else {
+				temp2.style.display ="inline-block";
+				temp2.selectedIndex = 0;
+				temp2.length = temp1list.part.length +1;
+				
+				temp2.options[0].innerHTML = "(" + ["章","イベント","キャラクター","区分"][temp1index] + "を選択)";
+				for(i=0; i<temp1list.part.length; i++){
+					temp2.options[i+1].innerHTML = temp1list.part[i].name;
+				}
+			}
+			return false;
+		
+		case 2:
+			if (temp2index < 0) return false;
+			const TimeOutputStart = performance.now();
+			
+			let coltemp = "none";
+			if('color' in temp1list){ coltemp = temp1list.color;}
+			if('color' in temp1list.part[temp2index]){ coltemp = temp1list.part[temp2index].color;}
+			
+			//◆データを出力
+			const DataSet = temp1list.part[temp2index].part;
+			
+			const html = DataSet.map( story => {
+				const tags = story.tags.map( tag => DrawCharName(tag) ).join('');
+				const color = ('color' in story ? story.color : coltemp);
+			
+				return `
+				<article class="story-container article-color-${color}">
+					<div class="story-title">${story.title}</div>
+					<div class="story-key">あらすじ</div>
+					<div class="story-information">${story.desc}</div>
+					<div class="story-key row2">登場人物</div>
+					<div class="story-information row2">${tags}</div>
+					${('memo' in story ? `<div class="story-key row2">メモ</div><div class="story-information row2">${story.memo}</div>` : '')}
+				</article>`;
+			}).join('');
+
+			document.getElementById("OutputArea").innerHTML = html;
+			document.getElementById("OutputArea").scrollTop = 0;
+			
+			if(isDebugMode){
+				const TimeOutputEnd = performance.now();
+				console.log(document.getElementById('N1').value + ' - ' + document.getElementById('N2').value + '\n'
+				+ '描画処理時間: ' + (TimeOutputEnd - TimeOutputStart) + ' ミリ秒');
+			}
+			return;
+		}
+	return false;
+}
+
+//■初期化処理
+function initialize () {
+	const TimeOutputLoaded = performance.now();
+	//色データをCSSに追加
+	let AddedCSS = '\n';
+	Object.keys(TagData).forEach( function(key) {
+		AddedCSS += '.article-color-' + key + '{\n\t'
+		+ 'background-color: ' + getColor(TagData[key], 1.5) + ';\n\t'
+		+ 'border-color: ' + getColor(TagData[key], 1, 1) + '\n}\n';
+		if(TagData[key].style === "round"){
+			AddedCSS += '.button_' + key + '{\n\t'
+			+ 'background-color: ' + getColor(TagData[key], 2) + ';\n\t'
+			+ 'border-color: ' + getColor(TagData[key], 0) + '\n}\n';
+		}
+	});
+	document.querySelector('style').textContent += AddedCSS;
+	
+	//セレクトボックスに要素を追加
+	window['JSON-sifas-story'].forEach ( temp => {
+		const option = document.createElement("option");
+		option.text = temp.name;
+		document.getElementById("N1").appendChild(option);
+	});
+	
+	//警告解除
+	document.getElementById('OutputArea').classList.remove('output-box-default');
+	document.getElementById('OutputArea').innerHTML = `
+		<div style="padding: 10px; vertical-align: top; font-size: 130%; color: #666">
+			(上のプルダウンメニューから、期間を選んでください)
+		</div>`;
+	
+	//デバック用
+	if(isDebugMode) {
+		//描画時間の出力
+		const TimeOutputEnd = performance.now();
+		console.log(`リンクラ スクールアイドルコネクトまとめ\n読み込み： ${TimeOutputLoaded - TimeLoadingStart}ミリ秒\n初期化: ${TimeOutputEnd - TimeOutputLoaded}ミリ秒`);
+		
+		//◆データの不具合チェック
+		let isError = 0;
+		for(let temp1 of window['JSON-sifas-story']){
+			for(let temp2 of temp1.part){
+				
+				if(!('name' in temp2)){ //●タイトルが無い
+					console.log('[Error] 区分タイトルが未設定:\n\tLocation: ' + temp1.name);
+					console.log(temp2);
+					isError++;
+					break;
+				}
+				
+				for(let temp3 of temp2.part){
+					
+					if(!('title' in temp3)){ //●タイトルが無い
+						console.log('[Error] タイトルが未設定:\n\tLocation: ' + temp1.name + ' - ' + temp2.name);
+						isError++;
+						break;
+					}
+					const StoryLocation = '\tLocation: ' + temp1.name + ' - ' + temp2.name + ' - ' + temp3.title;
+					
+					if(!('desc' in temp3)){ //●あらすじが無い
+						console.log('[Error] あらすじが未設定:\n' + StoryLocation);
+						isError++;
+					}
+					if('tags' in temp3){
+						if(temp3['tags'].length === 0){ //●登場人物がない
+							console.log('[Error] 登場人物が未設定:\n' + StoryLocation);
+							isError++;
+						}
+						for(let temp4 of temp3.tags){
+							if(!(temp4 in TagData)){ //●登場人物が不正
+								console.log('[Error] 登場人物が不正 (' + temp4 + '):\n' + StoryLocation);
+								isError++;
+							}
+						}
+					} else { //●登場人物が未設定
+						console.log('[Error] 登場人物が未設定:\n' + StoryLocation);
+						isError++;
+					}
+				}
+			}
+		}
+		if(isError > 0){
+			alert('' + isError + '件のエラーが見つかりました。コンソールを確認してください。');
+		}
+	}
+}
