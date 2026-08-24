9cd5f48fcbc017d83dc0ba9541995f32db63380e
ratius <36814202+ratius@users.noreply.github.com>
2026-08-24 12:58:33 +0900

    Add files via upload

--- js/page/sort.js
+++ js/page/sort.js
@@ -335,7 +335,7 @@ const wrtieResult = () => {
 	}).join('');
 	const TweetText = `${SortTitle} をソートしたよ！\n${ResultForTweet}\n#ラブライブなんでもソート\nhttps://ratius.github.io/LLS/sort.html`;
 	const TweetURI = encodeURIComponent(TweetText).replaceAll("'", "%27");
-	const TweetButton = `<button id="TweetButton" onclick="window.open().location.href='https://x.com/intent/tweet?text=${TweetURI}'"><img src="img/x-logo-white.png" style="width:25px; height:25px">この結果をポスト</button>`;
+	const TweetButton = `<button id="TweetButton" onclick="window.open('https://x.com/intent/tweet?text=${TweetURI}')"><img src="img/x-logo-white.png" style="width:25px; height:25px">この結果をポスト</button>`;
 
 	document.getElementById("ResultArea").innerHTML = Header + Rankings + Footer + TweetButton;
 };
